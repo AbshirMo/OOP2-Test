e22725555f7f79413504e8f63ad4cd4803af75e2
Abshir <abshir.mohammed@strathmore.edu>
2020-05-03 12:45:05 +0300

    removed redundant file

---
+++ Interface.java
@@ -0,0 +1,14 @@
+/**
+*<h1>Interface</h1> 
+ *This is the interface component of the Remote Invocation
+ */
+
+	/**
+		* This abstarct method references the UI to be run
+	*/
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Interface extends Remote{
+	void animation() throws RemoteException;
+}
