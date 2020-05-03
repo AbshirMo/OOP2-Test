82a0c6d384b3b6245c66403866ca6086c66faff1
Abshir <abshir.mohammed@strathmore.edu>
2020-05-03 12:33:43 +0300

    First

---
+++ cat2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Client"},{"p":"<Unnamed>","l":"Interface"},{"p":"<Unnamed>","l":"Login"},{"p":"<Unnamed>","l":"Server"}]
